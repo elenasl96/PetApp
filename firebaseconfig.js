862c8b4d93162d95c4e1ab0c0d63883d89867016
matteogambella <55230920+matteogambella@users.noreply.github.com>
2020-11-12 17:02:25 +0100

    add firebase config file

---
+++ firebaseconfig.js
@@ -0,0 +1,17 @@
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+ var firebaseConfig = {
+    apiKey: "AIzaSyBE7tjvadykEtSDMZbxHCgKSREXqigAa7k",
+    authDomain: "petapp-64e34.firebaseapp.com",
+    databaseURL: "https://petapp-64e34.firebaseio.com",
+    projectId: "petapp-64e34",
+    storageBucket: "petapp-64e34.appspot.com",
+    messagingSenderId: "831172014060",
+    appId: "1:831172014060:web:63d4373102fe79a9341ac9",
+    measurementId: "G-TNDGL05D7L"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
