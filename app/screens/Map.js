a7b5d5593b0deb5500b16bab4f5b51c733bfb2f3
Elena <37112748+elenasl96@users.noreply.github.com>
2021-01-16 00:57:58 +0100

    add first marker and geomapping

--- app/screens/Map.js
+++ app/screens/Map.js
@@ -2,20 +2,57 @@ import React from "react";
 import MapView from "react-native-maps";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
 import * as Location from "expo-location";
+import { Marker } from "react-native-maps";
+import db from "../firebase/DatabaseManager";
 
 export default class MapScreen extends React.Component {
+  state = {
+    markers: [],
+    region: {
+      latitude: 45.464664,
+      longitude: 9.18854,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+  };
+
+  constructor() {
+    super();
+    db.getPlaces().then((placesIds) => {
+      placesIds.map((placeId) => {
+        db.getPlace(placeId).then((place) => {
+          let LatLng = [];
+          LatLng.push(place.region.latitude);
+          LatLng.push(place.region.longitude);
+          this.state.markers.push(place.region);
+          console.log("MARKERS");
+          console.log(this.state.markers);
+        });
+      });
+    });
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <MapView
-          initialRegion={{
-            latitude: 45.464664,
-            longitude: 9.18854,
-            latitudeDelta: 0.05,
-            longitudeDelta: 0.05,
-          }}
+          region={this.state.region}
           style={styles.mapStyle}
-        />
+          onRegionChangeComplete={this.onRegionChange.bind(this)}
+        >
+          {this.state.markers.map((marker, index) => (
+            <Marker
+              key={index}
+              coordinate={{ latitude: 45.495644999999996, longitude: 9.194631 }}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
+        </MapView>
       </View>
     );
   }
