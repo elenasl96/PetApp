52d3a98b71de122d46cd68bafd335eecb52dfbb9
Elena <37112748+elenasl96@users.noreply.github.com>
2021-03-16 23:14:05 +0100

    add icons in busines tabbar

--- app/Components/Navigators/TabBusinessNavigator.js
+++ app/Components/Navigators/TabBusinessNavigator.js
@@ -4,6 +4,8 @@ import HomeBusinessNavigator from "./HomeBusinessNavigator.js";
 import HomeNavigator from "./HomeNavigator.js";
 import LostPetsNavigator from "./LostPetsNavigator.js";
 import MapNavigator from "./MapNavigator.js";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const TabBusinessNavigator = createMaterialBottomTabNavigator(
   {
@@ -14,9 +16,29 @@ const TabBusinessNavigator = createMaterialBottomTabNavigator(
   },
   {
     initialRouteName: "Home",
-    activeColor: "white",
-    inactiveColor: "black",
+
     barStyle: { backgroundColor: "#F9844A" },
+
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "Home") {
+          iconName = focused ? "md-home" : "md-home";
+        } else if (routeName === "Map") {
+          iconName = focused ? "md-map" : "md-map";
+        } else if (routeName === "Lost") {
+          iconName = focused ? "md-paw" : "md-paw";
+        } else if (routeName === "Business") {
+          iconName = focused ? "md-business" : "md-business";
+        }
+        return <Ionicons name={iconName} size={24} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray",
+    },
   }
 );
 
