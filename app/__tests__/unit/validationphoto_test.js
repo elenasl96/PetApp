0ad1341b9c89b2635e0c8cb35d186b74d4214252
Matteo <matteogambella@gmail.com>
2021-06-12 13:03:36 +0200

    minor fix

--- app/__tests__/unit/validationphoto_test.js
+++ app/__tests__/unit/validationphoto_test.js
@@ -4,7 +4,7 @@ it('test photo validation 1', () => {
     expect(validator.isValid(validator.handlePhotoValidation("photo"))).toBeTruthy();
 })
 
-it('test photo validation 2', () => { // false because text must not be an empty string
+it('test photo validation 2', () => { // false because photo must not be null
     expect(validator.isValid(validator.handlePhotoValidation(null))).toBeFalsy();
 })
 
